5dcacf646da37a83f058e82bbd933438d1feb22b
AbboskhonShukurullaev <Abbsh24@gmail.com>
2022-02-13 20:31:09 +0500

    d

---
+++ AsyncImageTutorial/AsyncImageTutorial/AsyncImageTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageTutorialApp.swift
+//  AsyncImageTutorial
+//
+//  Created by Abbos Shukurullaev on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Abbos Shukurullaev on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
